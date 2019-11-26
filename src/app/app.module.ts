624ec93654b7a027b4123d9add189efa00af66b5
iscfgibarra <silentium77@hotmail.com>
2019-11-26 15:28:27 -0600

    Agregado el menu de material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { BrandLogoComponent } from './components/atoms/brand-logo/brand-logo.component';
 import { ItemMenuComponent } from './components/atoms/item-menu/item-menu.component';
+import { MainMenuComponent } from './components/molecules/main-menu/main-menu.component';
+import { MaterialModule } from './modules/material.module'
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, BrandLogoComponent, ItemMenuComponent ],
+  imports:      [ BrowserModule, FormsModule , MaterialModule],
+  declarations: [ AppComponent,  BrandLogoComponent, ItemMenuComponent, MainMenuComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
